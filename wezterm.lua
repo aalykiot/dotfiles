bf78a54122b79bf851ecfb419eb43e023fa3c490
Alex Alikiotis <alexalikiotis5@gmail.com>
2024-11-09 21:45:28 +0200

    updates ⚙️

--- wezterm.lua
+++ wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require 'wezterm'
+local wezterm = require "wezterm"
 local config = {}
 
 if wezterm.config_builder then
@@ -6,11 +6,16 @@ if wezterm.config_builder then
 end
 
 config.automatically_reload_config = true
-config.window_close_confirmation = 'NeverPrompt'
+config.window_close_confirmation = "NeverPrompt"
 config.font_size = 14
-config.font = wezterm.font('BlexMono Nerd Font')
-config.enable_tab_bar = false
-config.window_decorations = 'RESIZE'
-config.color_scheme = 'Tokyo Night'
+config.font = wezterm.font("BlexMono Nerd Font")
+config.enable_tab_bar = true
+config.window_decorations = "RESIZE"
+config.color_scheme = "Tokyo Night"
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.tab_and_split_indices_are_zero_based = true
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 30
 
 return config
