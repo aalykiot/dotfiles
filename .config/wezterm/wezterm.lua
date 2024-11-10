8f4ba44c488dfaa17f1813feaa3e4bd8efca4a84
Alex Alikiotis <alexalikiotis5@gmail.com>
2024-11-11 00:46:34 +0200

    Picking fira-code font for wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 local wez_action = wezterm.action
 local config = {}
 
@@ -7,13 +7,13 @@ if wezterm.config_builder then
 end
 
 config.automatically_reload_config = true
-config.window_close_confirmation = 'NeverPrompt'
+config.window_close_confirmation = "NeverPrompt"
 config.line_height = 1.2
 config.font_size = 14
-config.font = wezterm.font('Menlo')
+config.font = wezterm.font("Fira Code")
 config.enable_tab_bar = true
-config.window_decorations = 'RESIZE'
-config.color_scheme = 'Tokyo Night'
+config.window_decorations = "RESIZE"
+config.color_scheme = "Tokyo Night"
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.tab_and_split_indices_are_zero_based = true
@@ -23,7 +23,7 @@ config.tab_and_split_indices_are_zero_based = true
 local function move_pane(key, direction)
 	return {
 		key = key,
-		mods = 'LEADER',
+		mods = "LEADER",
 		action = wez_action.ActivatePaneDirection(direction),
 	}
 end
@@ -31,8 +31,8 @@ end
 local function split_pane(key, action)
 	return {
 		key = key,
-		mods = 'LEADER',
-		action = action({ domain = 'CurrentPaneDomain' }),
+		mods = "LEADER",
+		action = action({ domain = "CurrentPaneDomain" }),
 	}
 end
 
@@ -46,7 +46,7 @@ end
 local function move_cursor(key, direction_string)
 	return {
 		key = key,
-		mods = 'OPT',
+		mods = "OPT",
 		action = wez_action.SendString(direction_string),
 	}
 end
@@ -54,7 +54,7 @@ end
 local function close_pane(key)
 	return {
 		key = key,
-		mods = 'LEADER',
+		mods = "LEADER",
 		action = wez_action.CloseCurrentPane({ confirm = true }),
 	}
 end
@@ -62,48 +62,48 @@ end
 local function move_tab(key, direction)
 	return {
 		key = key,
-		mods = 'LEADER',
+		mods = "LEADER",
 		action = wez_action.ActivateTabRelative(direction),
 	}
 end
 
-config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 2000 }
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
 config.keys = {
 	{
-		key = 'a',
+		key = "a",
 		-- When we're in leader mode _and_ CTRL + A is pressed...
-		mods = 'LEADER|CTRL',
+		mods = "LEADER|CTRL",
 		-- Actually send CTRL + A key to the terminal
-		action = wez_action.SendKey({ key = 'a', mods = 'CTRL' }),
+		action = wez_action.SendKey({ key = "a", mods = "CTRL" }),
 	},
 	{
 		-- When we push LEADER + R activate the `resize_panes` keytable
-		key = 'r',
-		mods = 'LEADER',
+		key = "r",
+		mods = "LEADER",
 		action = wez_action.ActivateKeyTable({
-			name = 'resize_panes',
+			name = "resize_panes",
 			one_shot = false,
 			timeout_milliseconds = 2000,
 		}),
 	},
-	move_tab('n', 1),
-	move_tab('b', -1),
-	move_cursor('LeftArrow', '\x1bb'),
-	move_cursor('RightArrow', '\x1bf'),
-	move_pane('j', 'Down'),
-	move_pane('k', 'Up'),
-	move_pane('h', 'Left'),
-	move_pane('l', 'Right'),
-	split_pane('|', wez_action.SplitHorizontal),
-	split_pane('-', wez_action.SplitVertical),
-	close_pane('x'),
+	move_tab("n", 1),
+	move_tab("b", -1),
+	move_cursor("LeftArrow", "\x1bb"),
+	move_cursor("RightArrow", "\x1bf"),
+	move_pane("j", "Down"),
+	move_pane("k", "Up"),
+	move_pane("h", "Left"),
+	move_pane("l", "Right"),
+	split_pane("|", wez_action.SplitHorizontal),
+	split_pane("-", wez_action.SplitVertical),
+	close_pane("x"),
 }
 
 for i = 0, 9 do
 	-- LEADER + num to activate that tab
 	table.insert(config.keys, {
 		key = tostring(i),
-		mods = 'LEADER',
+		mods = "LEADER",
 		action = wez_action.ActivateTab(i),
 	})
 end
@@ -112,21 +112,21 @@ end
 
 config.key_tables = {
 	resize_panes = {
-		resize_pane('j', 'Down'),
-		resize_pane('k', 'Up'),
-		resize_pane('h', 'Left'),
-		resize_pane('l', 'Right'),
+		resize_pane("j", "Down"),
+		resize_pane("k", "Up"),
+		resize_pane("h", "Left"),
+		resize_pane("l", "Right"),
 	},
 }
 
 -- Effects
 
-wezterm.on('update-right-status', function(window, _)
-	local prefix = ''
-	local background = { Background = { Color = 'transparent' } }
+wezterm.on("update-right-status", function(window, _)
+	local prefix = ""
+	local background = { Background = { Color = "transparent" } }
 
 	if window:leader_is_active() then
-		prefix = ' ' .. utf8.char(0x26A1) -- ⚡ emoji
+		prefix = " " .. utf8.char(0x26A1) -- ⚡ emoji
 	end
 
 	-- Show indication when we're on the leader mode
