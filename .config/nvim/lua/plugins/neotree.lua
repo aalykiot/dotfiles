7b72982f7ca54a2bfb9b5a283389607f366c4d04
Alex Alikiotis <alexalikiotis5@gmail.com>
2024-11-14 00:39:16 +0200

    Displaying hidden files in neotree

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -8,6 +8,13 @@ return  {
   },
   config = function()
     require('neo-tree').setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
       window = {
         position = 'left',
         width = 30,
