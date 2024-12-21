3e21f9198a362eed62ee752869b3942b7ef3c84c
Alex Alikiotis <alexalikiotis5@gmail.com>
2024-12-21 22:08:49 +0200

    Updating nvim suggestion settings

--- .config/nvim/lua/plugins/suggestions.lua
+++ .config/nvim/lua/plugins/suggestions.lua
@@ -4,33 +4,41 @@ return {
         'hrsh7th/cmp-nvim-lsp',
         'hrsh7th/cmp-path',
         'hrsh7th/cmp-buffer',
-        'hrsh7th/vim-vsnip',
-        'hrsh7th/cmp-vsnip',
     },
     config = function()
         local cmp = require('cmp')
         cmp.setup({
             mapping = cmp.mapping.preset.insert({
-                ['<C-Space>'] = cmp.mapping.complete(),
                 ['<C-j>'] = cmp.mapping.select_next_item(),
                 ['<C-k>'] = cmp.mapping.select_prev_item(),
                 ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
                 ['<Tab>'] = cmp.mapping.confirm({ select = true }),
                 ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-Space>'] = cmp.mapping.complete(),
             }),
             snippet = {
                 expand = function(args)
-                    vim.fn['vsnip#anonymous'](args.body)
+                    vim.snippet.expand(args.body)
                 end,
             },
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
-                { name = 'vsnip' },
                 { name = 'path' },
-                { name = 'crates' },
                 { name = 'buffer' },
+                { name = 'crates' },
             }),
+            completion = {
+                keyword_length = 2,
+            },
+            performance = {
+                max_view_entries = 7,
+            },
+            view = {
+                docs = {
+                    auto_open = false,
+                },
+            },
         })
     end,
 }
