cddee552b0b5b6d0e7d81604544c2753039da0ab
Alex Alikiotis <alexalikiotis5@gmail.com>
2025-01-09 23:32:48 +0200

    Updating neovim configuration

--- .config/nvim/lua/themes/gruvbox.lua
+++ .config/nvim/lua/themes/gruvbox.lua
@@ -6,6 +6,7 @@ return {
         vim.g.gruvbox_material_enable_italic = false
         vim.g.gruvbox_material_transparent_background = 1
         vim.g.gruvbox_material_disable_italic_comment = 1
+        vim.g.gruvbox_material_diagnostic_virtual_text = 'highlighted'
         vim.cmd.colorscheme('gruvbox-material')
     end,
 }
