69e1e6d19de3d35c264d18265d540e58c22ae47a
Alex Alikiotis <alexalikiotis5@gmail.com>
2026-05-20 14:20:09 +0300

    Fixing issues on neovim v0.12

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -40,11 +40,16 @@ return {
         -- vim.diagnostic.config({ virtual_text = false })
 
         -- Change diagnostics symbols
-        local signs = { Error = '󰅙', Info = '󰋼', Hint = '󰌵', Warn = '' }
-        for type, icon in pairs(signs) do
-            local hl = 'DiagnosticSign' .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
-        end
+        vim.diagnostic.config({
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = '󰅙',
+                    [vim.diagnostic.severity.WARN] = '',
+                    [vim.diagnostic.severity.HINT] = '󰌵',
+                    [vim.diagnostic.severity.INFO] = '󰋼',
+                },
+            },
+        })
 
         -- LSP servers we want to be installed
         local ensure_installed = {
