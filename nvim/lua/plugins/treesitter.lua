5361ef3cbf23a3dfa4c673d065b3a0107f86a2f0
Alexandros Alikiotis <alex.alikiotis@c2ddqjxjty.station>
2026-06-15 11:14:31 +0300

    Fixing nvim-treesitter issues on neovim 0.12

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,26 +1,36 @@
+-- https://www.qu8n.com/posts/treesitter-migration-guide-for-nvim-0-12
 return {
     'nvim-treesitter/nvim-treesitter',
     branch = 'main',
     lazy = false,
     build = ':TSUpdate',
-    config = function()
-        ---@diagnostic disable-next-line: missing-fields
-        require('nvim-treesitter.configs').setup({
-            ensure_installed = {
-                'bash',
-                'lua',
-                'luadoc',
-                'jsdoc',
-                'json',
-                'javascript',
-                'typescript',
-                'rust',
-            },
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
+    init = function()
+        -- Enable highlighting
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function()
+                -- Enable treesitter highlighting and disable regex syntax
+                pcall(vim.treesitter.start)
+                -- Enable treesitter-based indentation
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
         })
+        -- Ensure installed parsers
+        local ensure_installed = {
+            'bash',
+            'lua',
+            'luadoc',
+            'jsdoc',
+            'json',
+            'javascript',
+            'typescript',
+            'rust',
+        }
+        local alreadyInstalled = require('nvim-treesitter.config').get_installed()
+        local parsersToInstall = vim.iter(ensure_installed)
+            :filter(function(parser)
+                return not vim.tbl_contains(alreadyInstalled, parser)
+            end)
+            :totable()
+        require('nvim-treesitter').install(parsersToInstall)
     end,
 }
